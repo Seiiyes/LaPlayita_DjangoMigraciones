7e493e7e675801604bba960d1d403ec6e3243a3f
Michaelda361 <michaeldaramirez117@gmail.com>
2025-12-10 10:04:13 -0500

    preparacion despliegue

--- la_playita_project/build.sh
+++ la_playita_project/build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-# Este script es SOLO para Render.com - NO ejecutar en Windows
-
 set -o errexit
 
 pip install --upgrade pip
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py collectstatic --noinput
